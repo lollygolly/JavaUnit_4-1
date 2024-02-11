9f6d2f41ce311e7d889028ad691aec6302a961fa
LollyGolly <valentina_96@inbox.ru>
2024-02-11 23:18:55 +0500

    tests were added build failed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,4 +12,21 @@ public class CashbackHackServiceTest {
         int actual = service.remain(600);
         Assert.assertEquals(actual, expected);
     }
+    @Test
+    public void shouldShowRemainderWhenAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowRemainderWhenAmountAfterBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1200);
+        Assert.assertEquals(actual, expected);
+    }
 }
