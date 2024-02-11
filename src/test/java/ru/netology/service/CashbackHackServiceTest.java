a74ce8c4bbb7ee57e243d11933f7135b6ffc54f2
LollyGolly <valentina_96@inbox.ru>
2024-02-12 00:09:36 +0500

    junit-platform was created and updated

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
     @Test
@@ -28,5 +29,23 @@ public class CashbackHackServiceTest {
         int expected = 0;
         int actual = service.remain(1200);
         Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowAmountForCashBackForJunit5() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(900);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowAmountForCashBackForJunit5WhenAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assertions.assertEquals(expected, actual);
     }
 }
